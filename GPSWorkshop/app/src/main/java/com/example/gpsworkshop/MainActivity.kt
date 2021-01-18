d7f1cbc7d0db11390b4596c2383eac91eb5f01d8
ABonkowski-ITM <56539925+ABonkowski-ITM@users.noreply.github.com>
2021-01-18 10:55:16 +0100

    Update Code

--- GPSWorkshop/app/src/main/java/com/example/gpsworkshop/MainActivity.kt
+++ GPSWorkshop/app/src/main/java/com/example/gpsworkshop/MainActivity.kt
@@ -1,58 +1,72 @@
 package com.example.gps
 
 import android.Manifest
+import android.Manifest.*
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Build
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.gpsworkshop.R
 import com.google.android.gms.location.LocationServices
 
+
 class MainActivity : AppCompatActivity() {
-    var locationButton: Button = findViewById(R.id.location_btn)
-    var locationTextView: TextView = findViewById(R.id.location_txt)
+
+    val btn_Location  = findViewById<Button>(R.id.location_btn)
+    var TextViewLocation = findViewById<TextView>(R.id.location_txt)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initViews()
+        btn_Location.setOnClickListener { checkingPermissons() }
     }
 
-    private fun initViews() {
-        locationButton.setOnClickListener(View.OnClickListener { view: View? -> onClickCheckPermission() })
-    }
 
-    private fun onClickCheckPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            updateLocation()
-        } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PackageManager.PERMISSION_GRANTED)
+    private fun checkingPermissons() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+                ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED -> {
+                    Toast.makeText(applicationContext, "Permission Granted", Toast.LENGTH_SHORT).show()
+                    updateLocation()
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                    showDialog()
+                }
+                else -> {
+                    requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PackageManager.PERMISSION_GRANTED)
+                }
             }
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            updateLocation()
         }
     }
 
     @SuppressLint("MissingPermission")
     private fun updateLocation() {
         val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location -> onLocationReceived(location) }
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location : Location -> onLocationReceived(location) }
     }
 
-    private fun onLocationReceived(location: Location) {
-        val locationText = "Test"
-        locationTextView.text = locationText
+    private fun onLocationReceived(location: Location){
+        val location_text = location.latitude.toString() + "|" + location.longitude.toString()
+        TextViewLocation.text = location_text
+    }
 
+    private fun showDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.apply {
+            setMessage("Permission to access your GPS is required for this App")
+            setTitle("Permission required")
+            setPositiveButton("OK") { dialog, which ->
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PackageManager.PERMISSION_GRANTED)
+            }
+        }
     }
 }
